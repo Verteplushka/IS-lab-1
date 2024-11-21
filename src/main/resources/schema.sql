b7300d9fdde8e7363e5bf5d1add8bc2dd5123f65
Verteplushka <kolbacin_vlad@mail.ru>
2024-11-21 22:01:33 +0300

    Changed enums to stupid varchars (whyyyyy)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,11 @@
+-- Удаление зависимых таблиц
+DROP TABLE IF EXISTS product CASCADE;
+DROP TABLE IF EXISTS coordinates CASCADE;
+DROP TABLE IF EXISTS organization CASCADE;
+DROP TABLE IF EXISTS person CASCADE;
+DROP TABLE IF EXISTS address CASCADE;
+DROP TABLE IF EXISTS location CASCADE;
+
 CREATE TABLE Coordinates (
                              id SERIAL PRIMARY KEY,
                              x INTEGER NOT NULL,
@@ -18,20 +26,6 @@ CREATE TABLE Address (
                          zip_code VARCHAR NOT NULL
 );
 
-CREATE TYPE UnitOfMeasure AS ENUM ('CENTIMETERS', 'SQUARE_METERS', 'MILLILITERS');
-
-CREATE TYPE OrganizationType AS ENUM (
-    'COMMERCIAL',
-    'PUBLIC',
-    'TRUST',
-    'PRIVATE_LIMITED_COMPANY',
-    'OPEN_JOINT_STOCK_COMPANY'
-);
-
-CREATE TYPE Color AS ENUM ('BLACK', 'BLUE', 'WHITE', 'BROWN');
-
-CREATE TYPE Country AS ENUM ('SPAIN', 'ITALY', 'NORTH_KOREA');
-
 CREATE TABLE Organization (
                               id BIGSERIAL PRIMARY KEY,
                               name VARCHAR NOT NULL CHECK (name <> ''),
@@ -39,17 +33,17 @@ CREATE TABLE Organization (
                               annual_turnover REAL NOT NULL CHECK (annual_turnover > 0),
                               employees_count INTEGER CHECK (employees_count > 0),
                               full_name VARCHAR(1668) UNIQUE NOT NULL,
-                              type OrganizationType NOT NULL
+                              type VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE Person (
                         id SERIAL PRIMARY KEY,
                         name VARCHAR NOT NULL CHECK (name <> ''),
-                        eye_color Color NOT NULL,
-                        hair_color Color,
+                        eye_color VARCHAR(20) NOT NULL,
+                        hair_color VARCHAR(20),
                         location_id INTEGER NOT NULL REFERENCES Location(id),
                         weight DOUBLE PRECISION NOT NULL CHECK (weight > 0),
-                        nationality Country NOT NULL
+                        nationality VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE Product (
@@ -57,11 +51,19 @@ CREATE TABLE Product (
                          name VARCHAR NOT NULL CHECK (name <> ''),
                          coordinates_id INTEGER NOT NULL REFERENCES Coordinates(id),
                          creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                         unit_of_measure UnitOfMeasure,
+                         unit_of_measure VARCHAR(20),
                          manufacturer_id BIGINT REFERENCES Organization(id),
                          price BIGINT NOT NULL CHECK (price > 0),
                          manufacture_cost INTEGER,
                          rating BIGINT CHECK (rating > 0),
                          part_number VARCHAR(44) CHECK (length(part_number) >= 25 AND length(part_number) <= 44),
                          owner_id INTEGER NOT NULL REFERENCES Person(id)
+);
+
+-- Создание таблицы users_is
+CREATE TABLE users_is (
+                          id SERIAL PRIMARY KEY,        -- Автоинкрементируемый идентификатор
+                          login VARCHAR(255) NOT NULL UNIQUE, -- Уникальный логин
+                          password VARCHAR(255) NOT NULL,     -- Пароль
+                          role VARCHAR(20) NOT NULL
 );
